1f33a591227fdef9fa73480feffdd9b3ad37ba15
GitHub Action <action@github.com>
2023-02-20 09:31:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/ioexceptions/docgenerate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ioexceptions/docgenerate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ioexceptions.docgenerate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ioexceptions.docgenerate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ioexceptions/docgenerate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ioexceptions/docgenerate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ioexceptions.docgenerate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ioexceptions.docgenerate.MyBundle
 
 class MyProjectService(project: Project) {
 
